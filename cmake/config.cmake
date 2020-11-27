158e3834d4222a50db3c41cdadbdb369c63a4ed4
SirHall <sirhallstein@gmail.com>
2020-11-27 16:41:54 +1000

    Setup config.cmake with volt_event v0.0.11 and volt_net v0.0.1

---
+++ cmake/config.cmake
@@ -0,0 +1,11 @@
+hunter_config(volt_event
+    VERSION 0.0.11
+    URL "https://github.com/SirHall/volt_event/archive/v0.0.11.tar.gz"
+    SHA1 "c49b16dff3603ebb52573ad796d11afee3b6a0f0"
+)
+
+hunter_config(volt_net
+    VERSION 0.0.1
+    URL "https://github.com/SirHall/volt_net/archive/v0.0.1.tar.gz"
+    SHA1 "f1d7a1f3d0a4619c3e796734c265f4ac0fc420b3"
+)
