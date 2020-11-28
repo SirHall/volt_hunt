4b53715225ad8b7486abe239d842183495c414c8
SirHall <sirhallstein@gmail.com>
2020-11-28 11:40:17 +1000

    Add volt_gfx v0.0.2

--- volt_hunt_repo.cmake
+++ volt_hunt_repo.cmake
@@ -9,3 +9,10 @@ hunter_config(volt_net
     URL "https://github.com/SirHall/volt_net/archive/v0.0.1.tar.gz"
     SHA1 "f1d7a1f3d0a4619c3e796734c265f4ac0fc420b3"
 )
+
+hunter_config(volt_gfx
+    VERSION 0.0.2
+    URL "https://github.com/SirHall/volt_gfx/archive/v0.0.2.tar.gz"
+    SHA1 "f251e6b04da3ff08d8481a065db04b5777f219d6"
+)
+
